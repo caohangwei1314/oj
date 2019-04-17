48844800da23e1c503b39c325460eaeb14fbc6b0
PinuoC <104079381@qq.com>
2019-04-17 19:13:56 +0800

    fix users upload images bug

--- src/main/java/com/noi/oj/service/impl/UsersImpl.java
+++ src/main/java/com/noi/oj/service/impl/UsersImpl.java
@@ -4,10 +4,7 @@ import com.noi.oj.config.ServerConfig;
 import com.noi.oj.dao.UsersMapper;
 import com.noi.oj.domain.Users;
 import com.noi.oj.service.UsersService;
-import com.noi.oj.utils.IpUtil;
-import com.noi.oj.utils.PageBean;
-import com.noi.oj.utils.Sha2Util;
-import com.noi.oj.utils.SystemConstant;
+import com.noi.oj.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -71,6 +68,7 @@ public class UsersImpl implements UsersService {
     @Override
     public int updateByPrimaryKey(Users record)
     {
+        record.setImage(null);
         return usersMapper.updateByPrimaryKey(record);
     }
 
@@ -80,56 +78,15 @@ public class UsersImpl implements UsersService {
         String OSName = System.getProperty("os.name");
         String profilePath = OSName.toLowerCase().startsWith("win") ? SystemConstant.WINDOWS_PROFILES_PATH
                 : SystemConstant.LINUX_PROFILES_PATH;
-        if(!profile.isEmpty()){
-            Long userId = Long.parseLong(request.getAttribute("userId").toString());
-            Users users = selectByPrimaryKey(userId);
-            String profilePathAndNameDB = users.getImage();
-            // 默认以原来的头像名称为新头像的名称，这样可以直接替换掉文件夹中对应的旧头像
-            String newProfileName = profilePathAndNameDB;
-            // 若头像名称不存在
+        Long userId = Long.parseLong(request.getAttribute("userId").toString());
+        Users users = usersMapper.selectByPrimaryKey(userId);
+        if(users.getImage()==null || "".equals(users.getImage())){
             String[] originName = profile.getOriginalFilename().split("\\.");
-            String suffix = originName[originName.length-1];
-            if(profilePathAndNameDB == null || "".equals(profilePathAndNameDB)){
-                newProfileName = UUID.randomUUID().toString() + System.currentTimeMillis() + "." + suffix;
-                // 路径存库
-                users.setImage(newProfileName);
-                updateByPrimaryKeySelective(users);
-            }else{
-                String[] profiles = profilePathAndNameDB.split("\\.");
-                if(!suffix.equals(profiles[profiles.length-1])){
-                    File file = new File(profilePath + profilePathAndNameDB);
-                    if(file.exists())
-                        file.delete();
-                    newProfileName = profiles[0] + "." + suffix;
-                    users.setImage(newProfileName);
-                    updateByPrimaryKeySelective(users);
-                }
-            }
-            //磁盘保存
-            BufferedOutputStream out = null;
-            newProfileName = profilePath + newProfileName;
-            try {
-                File folder = new File(profilePath);
-                if(!folder.exists()){
-                    folder.mkdirs();
-                }
-                out = new BufferedOutputStream(new FileOutputStream(newProfileName));
-                // 写入新文件
-                out.write(profile.getBytes());
-                out.flush();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    out.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return true;
-        }else{
-            return false;
+            String suffix = originName[originName.length - 1];
+            users.setImage(UUID.randomUUID().toString() + System.currentTimeMillis() + "." + suffix);
+            updateByPrimaryKeySelective(users);
         }
+        return UploadUtils.setProductProfile(profile,users.getImage(),profilePath);
     }
 
     public String getUrl(String path){
